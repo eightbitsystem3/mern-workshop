495e6ee4acbc084719ad26cfd97775285d182a74
saurabhsharmaj <sourabh.sharma0185@gmail.com>
2026-04-06 16:15:27 +0530

    refactor files for argocd.

--- gitops/argocd/setup.bat
+++ gitops/argocd/setup.bat
@@ -1,5 +1,5 @@
 kubectl create namespace argocd
-kubectl apply -f argocd-deployment.yaml
-kubectl apply -f argocd-service.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode
+#kubectl port-forward svc/argocd-server -n argocd 8080:443
+#kubectl get secret argocd-initial-admin-secret -n argocd -o go-template="{{.data.password | base64decode}}"
