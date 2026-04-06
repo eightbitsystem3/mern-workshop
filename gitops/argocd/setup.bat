e3e27697759bef7ccb75cc09fc6d6db870b51d69
saurabhsharmaj <sourabh.sharma0185@gmail.com>
2026-04-06 16:34:49 +0530

    update setup batch file.

--- gitops/argocd/setup.bat
+++ gitops/argocd/setup.bat
@@ -3,3 +3,4 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 
 #kubectl port-forward svc/argocd-server -n argocd 8080:443
 #kubectl get secret argocd-initial-admin-secret -n argocd -o go-template="{{.data.password | base64decode}}"
+#kubectl port-forward svc/mern-frontend-service -n mern 80:80
